66ad61e1ea10a7b1d832b64214d7d181e690e0df
GreenNope <tlash1000@gmail.com>
2017-08-11 14:33:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var ink = 0
+var bar = null
+var width = 0
 
 function tick() {
+  bar = document.getElementById("progressBar");
   ink = ink + 1
-  
+  width = (ink/10)*100
+  bar.width = width + "%"
 }
 
 while true do {
