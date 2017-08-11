c0d2a4abea1fb0561e24371c192c4b74a81389e3
GreenNope <tlash1000@gmail.com>
2017-08-11 15:56:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ function tick() {
   var bar = document.getElementById("progressBar")
   ink = ink + 1
   bar.style.width = (ink/10*100) + "%"
-  if ink/10*100 >= 90 then
+  if ink/10*100 >=100 {
     ink = 0
+    bar.style.width = "0%"
+  }
 }
 
 
